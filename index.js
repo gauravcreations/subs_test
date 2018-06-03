a52c896eaee737c8be88ed9d4e1b620cd1022c47
gauravcreations <5024016+gauravcreations@users.noreply.github.com>
2018-06-03 17:34:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ restService.post("/saySub", function(req, res) {
 res.setHeader("Content-Type","application/json");
 
  var subs = "21000";
- var afterLoad = require('after-load');
-afterLoad('https://google.com', function(html){
+ var afterLoad = require("after-load");
+afterLoad("https://google.com", function(html){
    console.log(html);
 });
 
